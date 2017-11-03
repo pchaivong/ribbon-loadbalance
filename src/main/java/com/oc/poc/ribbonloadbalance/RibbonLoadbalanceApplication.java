d562adf4ad28af147f870646b9a881c3898d0c9c
Prawit Chaivong <pchaivong@gmail.com>
2017-11-03 17:24:43 +0700

    EnabledDiscoveryClient

--- src/main/java/com/oc/poc/ribbonloadbalance/RibbonLoadbalanceApplication.java
+++ src/main/java/com/oc/poc/ribbonloadbalance/RibbonLoadbalanceApplication.java
@@ -2,8 +2,10 @@ package com.oc.poc.ribbonloadbalance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RibbonLoadbalanceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/oc/poc/ribbonloadbalance/config/RestTemplateConfiguration.java
+++ src/main/java/com/oc/poc/ribbonloadbalance/config/RestTemplateConfiguration.java
@@ -2,7 +2,6 @@ package com.oc.poc.ribbonloadbalance.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
